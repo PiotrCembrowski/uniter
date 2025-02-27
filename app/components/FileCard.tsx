6df8162ba7579b3c77f5b2413d9989935c37a192
Piotr Cembrowski <losttape@gmail.com>
2025-02-27 14:43:40 +0100

    working metric length

--- app/components/FileCard.tsx
+++ app/components/FileCard.tsx
@@ -11,12 +11,12 @@ export default function FileCard({
   metadata: string;
   unitType: string;
 }) {
-  const [baseUnit, setBaseUnit] = useState();
-  const [unit1, setUnit1] = useState<number>();
-  const [unit2, setUnit2] = useState<number>();
-  const [unit3, setUnit3] = useState<number>();
-  const [unit4, setUnit4] = useState<number>();
-  const [unit5, setUnit5] = useState<number>();
+  const [baseUnit, setBaseUnit] = useState<number>();
+  const [unit1, setUnit1] = useState<number>(0);
+  const [unit2, setUnit2] = useState<number>(0);
+  const [unit3, setUnit3] = useState<number>(0);
+  const [unit4, setUnit4] = useState<number>(0);
+  const [unit5, setUnit5] = useState<number>(0);
 
   useEffect(() => {
     console.log("baseUnit", baseUnit);
@@ -26,18 +26,43 @@ export default function FileCard({
     }
   }, [baseUnit, unitType]);
 
-  const Multiplier = (e) => {
-    console.log(e.target.value);
-    setUnit1(baseUnit);
-    setUnit2(baseUnit * 0.1);
-    setUnit3(baseUnit * 0.01);
-    setUnit4(baseUnit * 0.001);
-    setUnit5(baseUnit * 0.0001);
+  const Multiplier = (unit: number) => {
+    setUnit1(unit);
+    setUnit2(unit * 0.1);
+    setUnit3(unit * 0.01);
+    setUnit4(unit * 0.001);
+    setUnit5(unit * 0.000001);
   };
 
-  const RenderMetrics = (e) => {
-    console.log(e.target.value);
-    setUnit1(e.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = event.target.name;
+    const inputValue = Number(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    let baseValue: number;
+    switch (inputName) {
+      case "unit1":
+        baseValue = inputValue;
+        break;
+      case "unit2":
+        baseValue = inputValue * 10;
+        break;
+      case "unit3":
+        baseValue = inputValue * 100;
+        break;
+      case "unit4":
+        baseValue = inputValue * 1000;
+        break;
+      case "unit5":
+        baseValue = inputValue * 1000000;
+        break;
+      default:
+        baseValue = inputValue;
+    }
+
+    setBaseUnit(baseValue);
+    Multiplier(baseValue);
   };
 
   return (
@@ -47,30 +72,56 @@ export default function FileCard({
           <label>millimeter [mm]</label>
           <br />
           <input
+            name="unit1"
             className="border-2"
             type="text"
-            onChange={(e) => RenderMetrics(e)}
+            onChange={handleChange}
+            value={unit1}
           />
         </div>
         <div>
-          <label htmlFor="">centimiter [cm]</label>
+          <label htmlFor="">centimeter [cm]</label>
           <br />
-          <input className="border-2" type="text" />
+          <input
+            name="unit2"
+            className="border-2"
+            type="text"
+            onChange={handleChange}
+            value={unit2}
+          />
         </div>
         <div>
-          <label htmlFor="">decimiter [dm]</label>
+          <label htmlFor="">decimeter [dm]</label>
           <br />
-          <input className="border-2" type="text" />
+          <input
+            name="unit3"
+            className="border-2"
+            type="text"
+            onChange={handleChange}
+            value={unit3}
+          />
         </div>
         <div>
-          <label htmlFor="">miter [m]</label>
+          <label htmlFor="">meter [m]</label>
           <br />
-          <input className="border-2" type="text" />
+          <input
+            name="unit4"
+            className="border-2"
+            type="text"
+            onChange={handleChange}
+            value={unit4}
+          />
         </div>
         <div>
-          <label htmlFor="">kilomiter [km]</label>
+          <label htmlFor="">kilometer [km]</label>
           <br />
-          <input className="border-2" type="text" />
+          <input
+            name="unit5"
+            className="border-2"
+            type="text"
+            onChange={handleChange}
+            value={unit5}
+          />
         </div>
       </div>
       <div className="p-4">
