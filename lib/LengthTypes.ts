a2174b23222313b541ccc96d0249cb352a069379
Piotr Cembrowski <losttape@gmail.com>
2025-02-28 15:31:25 +0100

    adding units

--- lib/LengthTypes.ts
+++ lib/LengthTypes.ts
@@ -1,3 +1,22 @@
 export default function LengthTypes(system: string) {
-  return console.log(system);
+  if (system === "Metric System") {
+    return {
+      name: "Metric System",
+      unit1: 1,
+      unit2: 10,
+      unit3: 100,
+      unit4: 1000,
+      unit5: 1000000,
+    };
+  }
+  if (system === "Imperial System") {
+    return {
+      name: "Imperial System",
+      unit1: 1,
+      unit2: 12,
+      unit3: 36,
+      unit4: 1760,
+      unit5: 63360,
+    };
+  }
 }
