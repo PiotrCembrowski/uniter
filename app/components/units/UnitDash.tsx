121c924b891558079450e4693ec03da36329a821
Piotr Cembrowski <losttape@gmail.com>
2025-03-04 13:38:28 +0100

    props types

--- app/components/units/UnitDash.tsx
+++ app/components/units/UnitDash.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import UnitCard from "./UnitCard";
 
-const UnitDash = (unitType: string) => {
+interface UnitDashProps {
+  unitType: string;
+}
+
+const UnitDash: React.FC<UnitDashProps> = ({ unitType }) => {
   if (unitType === "length") {
-    return (
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        <UnitCard title="Metric System" unitType={unitType} />
-        <UnitCard title="Imperial System" unitType={unitType} />
-        <UnitCard title="Sea Metric" unitType={unitType} />
-        <UnitCard title="Astronomic Metric" unitType={unitType} />
-      </div>
-    );
   }
 
   return (
