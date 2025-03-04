c7aaf19715d46576a23ee6922d4e9712cead844e
Piotr Cembrowski <losttape@gmail.com>
2025-03-04 13:32:19 +0100

    type props

--- app/components/units/UnitDash.tsx
+++ app/components/units/UnitDash.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import UnitCard from "./UnitCard";
 
 const UnitDash = (unitType: string) => {
+  if (unitType === "length") {
+    return (
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <UnitCard title="Metric System" unitType={unitType} />
+        <UnitCard title="Imperial System" unitType={unitType} />
+        <UnitCard title="Sea Metric" unitType={unitType} />
+        <UnitCard title="Astronomic Metric" unitType={unitType} />
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
       <UnitCard title="Metric System" unitType={unitType} />
