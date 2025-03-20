0fbac3ecb73c7c8210897f1c9fdce34ea5cba434
Piotr Cembrowski <losttape@gmail.com>
2025-03-20 12:30:15 +0100

    adding newstate to the provider

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -2,6 +2,7 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { store, AppStore } from "../store/index";
+import { newState } from "../store/index";
 
 export default function StoreProvider({
   children,
@@ -12,6 +13,7 @@ export default function StoreProvider({
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = store();
+    storeRef.current.dispatch(newState(0));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
