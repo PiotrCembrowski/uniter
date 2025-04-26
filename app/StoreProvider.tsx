ff7c32fec5543fb71bf94107329d92a224968540
Piotr Cembrowski <losttape@gmail.com>
2025-04-26 11:36:20 +0200

    adding imperial state

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -2,7 +2,7 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { store, AppStore } from "../store/index";
-import { newState } from "../store/index";
+import { newMetricState, newImperialState } from "../store/index";
 
 export default function StoreProvider({
   children,
@@ -13,7 +13,8 @@ export default function StoreProvider({
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = store();
-    storeRef.current.dispatch(newState(0));
+    storeRef.current.dispatch(newMetricState(0));
+    storeRef.current.dispatch(newImperialState(0));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
