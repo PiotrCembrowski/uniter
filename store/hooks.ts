86baf658aacf68326aa8acdc15ceff8f8dc7d0e3
Piotr Cembrowski <losttape@gmail.com>
2025-05-09 15:15:09 +0200

    refactoring dashstate

--- store/hooks.ts
+++ store/hooks.ts
@@ -5,19 +5,9 @@ import {
   useStore,
 } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "./index";
-import type {
-  RootDashState,
-  AppDashDispatch,
-  AppDashStore,
-} from "./dashOperator";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 // export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
-
-export const useAppDashDsipatch: () => AppDashDispatch = useDispatch;
-export const useAppDashSelector: TypedUseSelectorHook<RootDashState> =
-  useSelector;
-export const useAppDashStore: () => AppDashStore = useStore;
