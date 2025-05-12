1a4577c2afe309d6b13c6c000ade2cf8c1e8535b
Piotr Cembrowski <losttape@gmail.com>
2025-05-12 14:30:07 +0200

    fixing css for flex

--- app/length/page.tsx
+++ app/length/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import SideBar from "../components/sidebar/SideBar";
+import Dashboard from "../components/Dashboard";
 
 const LengthDash = () => {
   return (
     <div>
       <SideBar />
+      <div>
+        <Dashboard />
+      </div>
     </div>
   );
 };
