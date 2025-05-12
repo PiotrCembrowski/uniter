59a3971922a7bb6803a536fb0d762b167606b702
Piotr Cembrowski <losttape@gmail.com>
2025-05-12 14:29:26 +0200

    css for flex

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -2,31 +2,14 @@
 
 import Header from "./header/Header";
 import LengthDash from "./units/LengthUnitDash";
-import MassUnitDash from "./units/MassUnitDash";
-import { useAppSelector } from "@/store/hooks";
 
 const Dashboard = () => {
-  const showLengthDash = useAppSelector((state) => state.dash?.showLengthDash);
-
-  console.log("showLengthDash", showLengthDash);
-
-  const showMassDash = useAppSelector((state) => state.dash?.showMassDash);
-
-  console.log("showMassDash", showMassDash);
-
-  let content;
-
-  if (showLengthDash) {
-    content = <LengthDash />;
-  }
-  if (showMassDash) {
-    content = <MassUnitDash />;
-  }
-
   return (
-    <div className="flex-1">
+    <div>
       <Header />
-      <div className="p-6">{content}</div>
+      <div className="p-6">
+        <LengthDash />
+      </div>
     </div>
   );
 };
