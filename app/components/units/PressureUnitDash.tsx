a1470a4e4260be7eef0b3b39f6ddaebb303fa5ae
Piotr Cembrowski <losttape@gmail.com>
2025-05-16 14:48:28 +0200

    adjusting component

--- app/components/units/PressureUnitDash.tsx
+++ app/components/units/PressureUnitDash.tsx
@@ -3,7 +3,7 @@ import PressureUnitCard from "./PressureUnitCard";
 const MassUnitDash = () => {
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-      <PressureUnitCard title="Mass metric" />
+      <PressureUnitCard />
     </div>
   );
 };
