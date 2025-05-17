b90acd8d3c3e1b7a3a2eb167db110123ff0379a7
Piotr Cembrowski <losttape@gmail.com>
2025-05-17 12:58:06 +0200

    adding types for units

--- app/components/sidebar/UnitTypes.tsx
+++ app/components/sidebar/UnitTypes.tsx
@@ -1,9 +1,77 @@
+type Units = {
+  unit1: number;
+  unit2: number;
+  unit3: number;
+  unit4: number;
+  unit5: number;
+  unit6: number;
+  unit7: number;
+  unit8: number;
+  unit9: number;
+  unit10: number;
+};
+
 const UnitTypes = ({ unitTable }: { unitTable: string }) => {
+  if (unitTable !== "pressure") {
+    return null; // Do not render anything if the unitTable is not "pressure"
+  }
+
   return (
     <div className="mt-2">
       <h5 className="cursor-pointer hover:text-gray-700 dark:hover:text-gray-300 ml-3 my-1">
-        {unitTable}
+        Jednostką ciśnienia w układzie SI jest Paskal [Pa] (Pascal).
       </h5>
+      <table>
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Jednostka</th>
+            <th className="px-4 py-2">Przelicznik</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border px-4 py-2">Pascal</td>
+            <td className="border px-4 py-2">1 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Bar</td>
+            <td className="border px-4 py-2">100000 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Kilopascal</td>
+            <td className="border px-4 py-2">1000 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Megapascal</td>
+            <td className="border px-4 py-2">1000000 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Hektopascal</td>
+            <td className="border px-4 py-2">100 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Milimetr słupa rtęci</td>
+            <td className="border px-4 py-2">133.322 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Centymetr słupa rtęci</td>
+            <td className="border px-4 py-2">1333.22 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Cal</td>
+            <td className="border px-4 py-2">6894.76 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Funt na cal kwadratowy</td>
+            <td className="border px-4 py-2">6894.76 Pa</td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Psi</td>
+            <td className="border px-4 py-2">6894.76 Pa</td>
+          </tr>
+          {/* Add more rows as needed */}
+        </tbody>
+      </table>
     </div>
   );
 };
