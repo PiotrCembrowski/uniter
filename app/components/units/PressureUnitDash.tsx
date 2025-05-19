1a413c7c84d814a1fa2d35d50925423fb121a36d
Piotr Cembrowski <losttape@gmail.com>
2025-05-19 12:58:16 +0200

    updating calcs

--- app/components/units/PressureUnitDash.tsx
+++ app/components/units/PressureUnitDash.tsx
@@ -9,29 +9,29 @@ const PressureUnitDash = () => {
 
   const [values, setValues] = useState<Values>({
     unit1: showState,
-    unit2: showState * 0.01,
-    unit3: showState * 0.00001,
-    unit4: showState * 0.000145038,
-    unit5: showState * 0.00000986923,
-    unit6: showState * 0.00000986923,
-    unit7: showState * 0.000145038,
-    unit8: showState * 0.00000986923,
-    unit9: showState * 0.00750062,
-    unit10: showState * 0.0208854,
+    unit2: showState / 100,
+    unit3: showState / 100000,
+    unit4: showState / 1000000,
+    unit5: showState / 9.80665,
+    unit6: showState / 98066.5,
+    unit7: showState / 101325,
+    unit8: showState / 133.322,
+    unit9: showState / 133.322,
+    unit10: showState / 6894.76,
   });
 
   useEffect(() => {
     setValues({
       unit1: showState,
-      unit2: showState * 0.01,
-      unit3: showState * 0.00001,
-      unit4: showState * 0.000145038,
-      unit5: showState * 0.00000986923,
-      unit6: showState * 0.00000986923,
-      unit7: showState * 0.000145038,
-      unit8: showState * 0.00000986923,
-      unit9: showState * 0.00750062,
-      unit10: showState * 0.0208854,
+      unit2: showState / 100,
+      unit3: showState / 100000,
+      unit4: showState / 1000000,
+      unit5: showState / 9.80665,
+      unit6: showState / 98066.5,
+      unit7: showState / 101325,
+      unit8: showState / 133.322,
+      unit9: showState / 133.322,
+      unit10: showState / 6894.76,
     });
   }, [showState]);
 
