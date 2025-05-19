a3da61db40cdcab4cf849c9f6ea0ac3aac1917b5
Piotr Cembrowski <losttape@gmail.com>
2025-05-19 15:10:13 +0200

    updating units numbers

--- app/components/units/TimeUnitDash.tsx
+++ app/components/units/TimeUnitDash.tsx
@@ -8,28 +8,26 @@ const TimeUnitDash = () => {
   const timeState = useAppSelector((state) => state.timeSlice.units);
 
   const [values, setValues] = useState<Values>({
-    unit1: timeState,
-    unit2: timeState / 1e-12,
-    unit3: timeState / 1e-9,
-    unit4: timeState / 1e-6,
-    unit5: timeState / 0.001,
-    unit6: timeState / 1,
-    unit7: timeState / 60,
-    unit8: timeState / 3600,
-    unit9: timeState / 86400,
+    unit1: timeState / 1e-12,
+    unit2: timeState / 1e-9,
+    unit3: timeState / 1e-6,
+    unit4: timeState / 0.001,
+    unit5: timeState,
+    unit6: timeState / 60,
+    unit7: timeState / 3600,
+    unit8: timeState / 86400,
   });
 
   useEffect(() => {
     setValues({
-      unit1: timeState,
-      unit2: timeState / 1e-12,
-      unit3: timeState / 1e-9,
-      unit4: timeState / 1e-6,
-      unit5: timeState / 0.001,
-      unit6: timeState / 1,
-      unit7: timeState / 60,
-      unit8: timeState / 3600,
-      unit9: timeState / 86400,
+      unit1: timeState / 1e-12,
+      unit2: timeState / 1e-9,
+      unit3: timeState / 1e-6,
+      unit4: timeState / 0.001,
+      unit5: timeState,
+      unit6: timeState / 60,
+      unit7: timeState / 3600,
+      unit8: timeState / 86400,
     });
   }, [timeState]);
 
