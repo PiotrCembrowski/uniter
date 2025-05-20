96e560f32cf66ee58913f53d7d8a60bf15338174
Piotr Cembrowski <losttape@gmail.com>
2025-05-20 15:37:06 +0200

    energy page

---
+++ app/energy/page.tsx
@@ -0,0 +1,15 @@
+import SideBar from "../components/sidebar/SideBar";
+import Dashboard from "../components/Dashboard";
+
+const EnergyDash = () => {
+  return (
+    <div className="flex">
+      <SideBar unitTable="energy" />
+      <div>
+        <Dashboard dashType="energy" />
+      </div>
+    </div>
+  );
+};
+
+export default EnergyDash;
