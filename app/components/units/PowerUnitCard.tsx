088838cd9b28846070b8c6e4b70ca3c053325fe0
Piotr Cembrowski <losttape@gmail.com>
2025-05-22 14:51:39 +0200

    powerunitdash update

---
+++ app/components/units/PowerUnitCard.tsx
@@ -0,0 +1,208 @@
+"use client";
+
+import { newPressureState } from "@/store";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+export type Values = {
+  unit1: number;
+  unit2: number;
+  unit3: number;
+  unit4: number;
+  unit5: number;
+  unit6: number;
+  unit7: number;
+};
+
+interface UnitCardProps {
+  value?: Values;
+}
+
+export default function PowerUnitCard({ value }: UnitCardProps) {
+  const [unit1Name, setUnit1Name] = useState<string>();
+  const [unit2Name, setUnit2Name] = useState<string>();
+  const [unit3Name, setUnit3Name] = useState<string>();
+  const [unit4Name, setUnit4Name] = useState<string>();
+  const [unit5Name, setUnit5Name] = useState<string>();
+  const [unit6Name, setUnit6Name] = useState<string>();
+  const [unit7Name, setUnit7Name] = useState<string>();
+  const [unit8Name, setUnit8Name] = useState<string>();
+  const [unit9Name, setUnit9Name] = useState<string>();
+  const [unit10Name, setUnit10Name] = useState<string>();
+  const [values, setValues] = useState<Values>({
+    unit1: 0,
+    unit2: 0,
+    unit3: 0,
+    unit4: 0,
+    unit5: 0,
+    unit6: 0,
+    unit7: 0,
+    unit8: 0,
+    unit9: 0,
+    unit10: 0,
+  });
+
+  useEffect(() => {
+    setUnit1Name("Pascal [1 Pa=N/m2]");
+    setUnit2Name("Hektopascal [hPa]");
+    setUnit3Name("Bar [bar]");
+    setUnit4Name("Newton/milimetr2 [1 N/mm2]");
+    setUnit5Name("Kilogram/metr2 [1 KG/m2]");
+    setUnit6Name("Technical atmosphere [at]");
+    setUnit7Name("Standard atmosphere [atm]");
+    setUnit8Name("Tor [Tr]");
+    setUnit9Name("Millimeter of mercury [mmHg]");
+    setUnit10Name("Psi [lbf/in2]");
+  }, []);
+
+  let baseValue: number;
+  const dispatch = useDispatch();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = event.target.name;
+    const inputValue = Number(event.target.value);
+    console.log(inputValue);
+
+    switch (inputName) {
+      case "unit1":
+        baseValue = inputValue;
+        break;
+      case "unit2":
+        baseValue = inputValue * 10;
+        break;
+      case "unit3":
+        baseValue = inputValue * 100;
+        break;
+      case "unit4":
+        baseValue = inputValue * 1000;
+        break;
+      case "unit5":
+        baseValue = inputValue * 1000000;
+        break;
+      default:
+        baseValue = inputValue;
+    }
+
+    dispatch(newPressureState(baseValue));
+  };
+
+  return (
+    <div className="group relative overflow-hidden rounded-lg border bg-white">
+      <div className="aspect-[4/3] p-3 flex flex-col justify-between space-y-2">
+        <div className="mb-8">
+          <h3 className="font-medium text-gray-900"></h3>
+        </div>
+        <div className="w-full">
+          <label>{unit1Name}</label>
+          <br />
+          <input
+            name="unit1"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit1}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit2Name}</label>
+          <br />
+          <input
+            name="unit2"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit2}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit3Name}</label>
+          <br />
+          <input
+            name="unit3"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit3}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit4Name}</label>
+          <br />
+          <input
+            name="unit4"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit4}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit5Name}</label>
+          <br />
+          <input
+            name="unit5"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit6Name}</label>
+          <br />
+          <input
+            name="unit6"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit7Name}</label>
+          <br />
+          <input
+            name="unit7"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit8Name}</label>
+          <br />
+          <input
+            name="unit8"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit9Name}</label>
+          <br />
+          <input
+            name="unit9"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+        <div>
+          <label htmlFor="">{unit10Name}</label>
+          <br />
+          <input
+            name="unit10"
+            className="border-2 w-full"
+            type="text"
+            onChange={handleChange}
+            value={value?.unit5}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/units/PowerUnitDash.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import PowerUnitCard, { Values } from "./PowerUnitCard";
+import { useAppSelector } from "@/store/hooks";
+
+const PowerUnitDash = () => {
+  const showState = useAppSelector((state) => state.pressureSlice.units);
+
+  const [values, setValues] = useState<Values>({
+    unit1: showState,
+    unit2: showState / 1000,
+    unit3: showState / 1000000,
+    unit4: showState / 745.7,
+    unit5: showState / 0.293071,
+    unit6: showState / 17.5843,
+    unit7: showState / 1055.06,
+  });
+
+  useEffect(() => {
+    setValues({
+      unit1: showState,
+      unit2: showState / 1000,
+      unit3: showState / 1000000,
+      unit4: showState / 745.7,
+      unit5: showState / 0.293071,
+      unit6: showState / 17.5843,
+      unit7: showState / 1055.06,
+    });
+  }, [showState]);
+
+  return (
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      <PowerUnitCard value={values} />
+    </div>
+  );
+};
+
+export default PowerUnitDash;
