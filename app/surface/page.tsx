9368108570a840a30f7a8da0b6d88534d613caaf
Piotr Cembrowski <losttape@gmail.com>
2025-05-27 13:45:52 +0200

    surface page added

---
+++ app/surface/page.tsx
@@ -0,0 +1,15 @@
+import SideBar from "../components/sidebar/SideBar";
+import Dashboard from "../components/Dashboard";
+
+const SurfaceDash = () => {
+  return (
+    <div className="flex">
+      <SideBar unitTable="surface" />
+      <div>
+        <Dashboard dashType="surface" />
+      </div>
+    </div>
+  );
+};
+
+export default SurfaceDash;
