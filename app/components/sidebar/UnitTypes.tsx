1e07967aa3522bf909960e3fdd2244a8db3c6d5b
Piotr Cembrowski <losttape@gmail.com>
2025-06-05 16:33:15 +0200

    table units added

--- app/components/sidebar/UnitTypes.tsx
+++ app/components/sidebar/UnitTypes.tsx
@@ -1,3 +1,4 @@
+import { json } from "stream/consumers";
 import * as lib from "../../../lib/unitsCalc.json";
 
 type Units = {
@@ -88,6 +89,58 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
     unit10: "",
   };
 
+  const units3: Units = {
+    unit1: 0,
+    unit2: 0,
+    unit3: 0,
+    unit4: 0,
+    unit5: 0,
+    unit6: 0,
+    unit7: 0,
+    unit8: 0,
+    unit9: 0,
+    unit10: 0,
+  };
+
+  const unitNames3: UnitNames = {
+    unit1: "",
+    unit2: "",
+    unit3: "",
+    unit4: "",
+    unit5: "",
+    unit6: "",
+    unit7: "",
+    unit8: "",
+    unit9: "",
+    unit10: "",
+  };
+
+  const units4: Units = {
+    unit1: 0,
+    unit2: 0,
+    unit3: 0,
+    unit4: 0,
+    unit5: 0,
+    unit6: 0,
+    unit7: 0,
+    unit8: 0,
+    unit9: 0,
+    unit10: 0,
+  };
+
+  const unitNames4: UnitNames = {
+    unit1: "",
+    unit2: "",
+    unit3: "",
+    unit4: "",
+    unit5: "",
+    unit6: "",
+    unit7: "",
+    unit8: "",
+    unit9: "",
+    unit10: "",
+  };
+
   const systemNames: SystemNames = {
     system1: "",
     system2: "",
@@ -136,14 +189,35 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
     units.unit7 = jsonFile.time.day[6].conversionFactor;
     units.unit8 = jsonFile.time.day[7].conversionFactor;
 
-    unitNames.unit1 = jsonFile.time.moreThanDay[0].name;
-    unitNames.unit2 = jsonFile.time.moreThanDay[1].name;
-    unitNames.unit3 = jsonFile.time.moreThanDay[2].name;
-    unitNames.unit4 = jsonFile.time.moreThanDay[3].name;
-    unitNames.unit5 = jsonFile.time.moreThanDay[4].name;
-    unitNames.unit6 = jsonFile.time.moreThanDay[5].name;
-    unitNames.unit7 = jsonFile.time.moreThanDay[6].name;
-    unitNames.unit8 = jsonFile.time.moreThanDay[7].name;
+    units2.unit1 = jsonFile.time.moreThanDay[0].conversionFactor;
+    units2.unit2 = jsonFile.time.moreThanDay[1].conversionFactor;
+    units2.unit3 = jsonFile.time.moreThanDay[2].conversionFactor;
+    units2.unit4 = jsonFile.time.moreThanDay[3].conversionFactor;
+    units2.unit5 = jsonFile.time.moreThanDay[4].conversionFactor;
+    units2.unit6 = jsonFile.time.moreThanDay[5].conversionFactor;
+    units2.unit7 = jsonFile.time.moreThanDay[6].conversionFactor;
+    units2.unit8 = jsonFile.time.moreThanDay[7].conversionFactor;
+
+    unitNames.unit1 = jsonFile.time.day[0].name;
+    unitNames.unit2 = jsonFile.time.day[1].name;
+    unitNames.unit3 = jsonFile.time.day[2].name;
+    unitNames.unit4 = jsonFile.time.day[3].name;
+    unitNames.unit5 = jsonFile.time.day[4].name;
+    unitNames.unit6 = jsonFile.time.day[5].name;
+    unitNames.unit7 = jsonFile.time.day[6].name;
+    unitNames.unit8 = jsonFile.time.day[7].name;
+
+    unitNames2.unit1 = jsonFile.time.moreThanDay[0].name;
+    unitNames2.unit2 = jsonFile.time.moreThanDay[1].name;
+    unitNames2.unit3 = jsonFile.time.moreThanDay[2].name;
+    unitNames2.unit4 = jsonFile.time.moreThanDay[3].name;
+    unitNames2.unit5 = jsonFile.time.moreThanDay[4].name;
+    unitNames2.unit6 = jsonFile.time.moreThanDay[5].name;
+    unitNames2.unit7 = jsonFile.time.moreThanDay[6].name;
+    unitNames2.unit8 = jsonFile.time.moreThanDay[7].name;
+
+    systemNames.system1 = "Day System";
+    systemNames.system2 = "More Than Day System";
   }
 
   if (unitTable === "pressure") {
@@ -179,6 +253,24 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
     units.unit6 = jsonFile.energy["Metric System"][5].conversionFactor;
     units.unit7 = jsonFile.energy["Metric System"][6].conversionFactor;
 
+    units2.unit1 = jsonFile.energy["Imperial System"][0].conversionFactor;
+    units2.unit2 = jsonFile.energy["Imperial System"][1].conversionFactor;
+    units2.unit3 = jsonFile.energy["Imperial System"][2].conversionFactor;
+    units2.unit4 = jsonFile.energy["Imperial System"][3].conversionFactor;
+    units2.unit5 = jsonFile.energy["Imperial System"][4].conversionFactor;
+
+    units3.unit1 = jsonFile.energy["Calories"][0].conversionFactor;
+    units3.unit2 = jsonFile.energy["Calories"][1].conversionFactor;
+
+    units4.unit1 = jsonFile.energy["Atomic Units"][0].conversionFactor;
+    units4.unit2 = jsonFile.energy["Atomic Units"][1].conversionFactor;
+    units4.unit3 = jsonFile.energy["Atomic Units"][2].conversionFactor;
+    units4.unit4 = jsonFile.energy["Atomic Units"][3].conversionFactor;
+    units4.unit5 = jsonFile.energy["Atomic Units"][4].conversionFactor;
+    units4.unit6 = jsonFile.energy["Atomic Units"][5].conversionFactor;
+    units4.unit7 = jsonFile.energy["Atomic Units"][6].conversionFactor;
+    units4.unit8 = jsonFile.energy["Atomic Units"][7].conversionFactor;
+
     unitNames.unit1 = jsonFile.energy["Metric System"][0].name;
     unitNames.unit2 = jsonFile.energy["Metric System"][1].name;
     unitNames.unit3 = jsonFile.energy["Metric System"][2].name;
@@ -186,6 +278,29 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
     unitNames.unit5 = jsonFile.energy["Metric System"][4].name;
     unitNames.unit6 = jsonFile.energy["Metric System"][5].name;
     unitNames.unit7 = jsonFile.energy["Metric System"][6].name;
+
+    unitNames2.unit1 = jsonFile.energy["Imperial System"][0].name;
+    unitNames2.unit2 = jsonFile.energy["Imperial System"][1].name;
+    unitNames2.unit3 = jsonFile.energy["Imperial System"][2].name;
+    unitNames2.unit4 = jsonFile.energy["Imperial System"][3].name;
+    unitNames2.unit5 = jsonFile.energy["Imperial System"][4].name;
+
+    unitNames3.unit1 = jsonFile.energy["Calories"][0].name;
+    unitNames3.unit2 = jsonFile.energy["Calories"][1].name;
+
+    unitNames4.unit1 = jsonFile.energy["Atomic Units"][0].name;
+    unitNames4.unit2 = jsonFile.energy["Atomic Units"][1].name;
+    unitNames4.unit3 = jsonFile.energy["Atomic Units"][2].name;
+    unitNames4.unit4 = jsonFile.energy["Atomic Units"][3].name;
+    unitNames4.unit5 = jsonFile.energy["Atomic Units"][4].name;
+    unitNames4.unit6 = jsonFile.energy["Atomic Units"][5].name;
+    unitNames4.unit7 = jsonFile.energy["Atomic Units"][6].name;
+    unitNames4.unit8 = jsonFile.energy["Atomic Units"][7].name;
+
+    systemNames.system1 = "Metric System";
+    systemNames.system2 = "Imperial System";
+    systemNames.system3 = "Calories";
+    systemNames.system4 = "Atomic Units";
   }
 
   return (
@@ -298,7 +413,7 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
       {systemNames.system2 && (
         <h2 className="font-bold text-2xl">{systemNames.system2}</h2>
       )}
-      {unitNames2.unit1 && (
+      {systemNames.system2 && (
         <table className="mt-3">
           <thead className="">
             <tr>
@@ -394,6 +509,232 @@ const UnitTypes = ({ unitTable }: { unitTable: UnitKey }) => {
                 </td>
                 <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
                   {units2.unit10}
+                </td>
+              </tr>
+            )}
+            {/* Add more rows as needed */}
+          </tbody>
+        </table>
+      )}
+      {systemNames.system3 && (
+        <h2 className="font-bold text-2xl">{systemNames.system3}</h2>
+      )}
+      {unitNames3.unit1 && (
+        <table className="mt-3">
+          <thead className="">
+            <tr>
+              <th className="px-4 py-2">Jednostka</th>
+              <th className="px-4 py-2">Przelicznik</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-[#4F3E8C] px-4 py-2">
+                {unitNames3.unit1}
+              </td>
+              <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                {units3.unit1}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-[#4F3E8C] px-4 py-2">
+                {unitNames3.unit2}
+              </td>
+              <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                {units3.unit2}
+              </td>
+            </tr>
+            {unitNames3.unit3 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit3}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit3}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit4 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit4}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit4}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit5 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit5}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit5}
+                </td>
+              </tr>
+            )}
+
+            {unitNames3.unit6 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit6}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit6}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit7 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit7}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit7}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit8 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit8}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit8}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit9 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit9}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit9}
+                </td>
+              </tr>
+            )}
+            {unitNames3.unit10 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames3.unit10}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units3.unit10}
+                </td>
+              </tr>
+            )}
+            {/* Add more rows as needed */}
+          </tbody>
+        </table>
+      )}
+      {systemNames.system4 && (
+        <h2 className="font-bold text-2xl">{systemNames.system3}</h2>
+      )}
+      {unitNames4.unit1 && (
+        <table className="mt-3">
+          <thead className="">
+            <tr>
+              <th className="px-4 py-2">Jednostka</th>
+              <th className="px-4 py-2">Przelicznik</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-[#4F3E8C] px-4 py-2">
+                {unitNames4.unit1}
+              </td>
+              <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                {units4.unit1}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-[#4F3E8C] px-4 py-2">
+                {unitNames4.unit2}
+              </td>
+              <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                {units4.unit2}
+              </td>
+            </tr>
+            {unitNames4.unit3 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit3}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit3}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit4 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit4}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit4}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit5 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit5}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit5}
+                </td>
+              </tr>
+            )}
+
+            {unitNames4.unit6 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit6}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit6}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit7 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit7}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit7}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit8 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit8}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit8}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit9 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit9}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit9}
+                </td>
+              </tr>
+            )}
+            {unitNames4.unit10 && (
+              <tr>
+                <td className="border border-[#4F3E8C] px-4 py-2">
+                  {unitNames4.unit10}
+                </td>
+                <td className="border border-[#4F3E8C] px-4 py-2 text-[#46A66F] font-bold">
+                  {units4.unit10}
                 </td>
               </tr>
             )}
