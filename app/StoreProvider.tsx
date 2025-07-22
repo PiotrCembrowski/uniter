1d829f489c45dbd31291a2e4b0f85f8e9dfb3747
Piotr Cembrowski <losttape@gmail.com>
2025-07-22 17:07:51 +0200

    cleaning storeRef

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -2,8 +2,6 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { store, AppStore } from "../store/index";
-import { newLengthMetricState, newLengthImperialState } from "../store/index";
-import Big from "big.js";
 
 export default function StoreProvider({
   children,
@@ -14,8 +12,6 @@ export default function StoreProvider({
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = store();
-    storeRef.current.dispatch(newLengthMetricState(new Big(0)));
-    storeRef.current.dispatch(newLengthImperialState(new Big(0)));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
