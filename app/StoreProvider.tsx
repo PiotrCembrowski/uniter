456c6bc08d4c02412fd82ad62db6efaa81f17da2
Piotr Cembrowski <losttape@gmail.com>
2025-07-22 14:54:00 +0200

    fixing imports of length reducers

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -2,7 +2,8 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { store, AppStore } from "../store/index";
-import { newMetricState, newImperialState } from "../store/index";
+import { newLengthMetricState, newLengthImperialState } from "../store/index";
+import Big from "big.js";
 
 export default function StoreProvider({
   children,
@@ -13,8 +14,8 @@ export default function StoreProvider({
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = store();
-    storeRef.current.dispatch(newMetricState(0));
-    storeRef.current.dispatch(newImperialState(0));
+    storeRef.current.dispatch(newLengthMetricState(new Big(0)));
+    storeRef.current.dispatch(newLengthImperialState(new Big(0)));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
