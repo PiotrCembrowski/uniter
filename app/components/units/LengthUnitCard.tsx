990f39fefc6a47d143fd09797413fbc9b981f592
Piotr Cembrowski <losttape@gmail.com>
2025-07-22 14:54:26 +0200

    updating jsx tree

--- app/components/units/LengthUnitCard.tsx
+++ app/components/units/LengthUnitCard.tsx
@@ -130,9 +130,8 @@ export default function MetricUnitCard({ title, value, digit }: UnitCardProps) {
           <input
             name="unit1"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
             onChange={handleChange}
-            value={value?.unit1}
+            value={value?.unit1 ? Big(value.unit1).round(digit).toString() : ""}
             data-title={title}
           />
         </div>
@@ -142,48 +141,60 @@ export default function MetricUnitCard({ title, value, digit }: UnitCardProps) {
           <input
             name="unit2"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
+            type="number"
             onChange={handleChange}
-            value={value?.unit2}
+            value={value?.unit2 ? Big(value.unit2).round(digit).toString() : ""}
             data-title={title}
           />
         </div>
-        <div>
-          <label htmlFor="">{unit3Name}</label>
-          <br />
-          <input
-            name="unit3"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit3}
-            data-title={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit4Name}</label>
-          <br />
-          <input
-            name="unit4"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit4}
-            data-title={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit5Name}</label>
-          <br />
-          <input
-            name="unit5"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit5}
-            data-title={title}
-          />
-        </div>
+        {unit3Name && (
+          <div>
+            <label htmlFor="">{unit3Name}</label>
+            <br />
+            <input
+              name="unit3"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              type="number"
+              onChange={handleChange}
+              value={
+                value?.unit3 ? Big(value.unit3).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
+        {unit4Name && (
+          <div>
+            <label htmlFor="">{unit4Name}</label>
+            <br />
+            <input
+              name="unit4"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              type="number"
+              onChange={handleChange}
+              value={
+                value?.unit4 ? Big(value.unit4).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
+        {unit5Name && (
+          <div>
+            <label htmlFor="">{unit5Name}</label>
+            <br />
+            <input
+              name="unit5"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              type="number"
+              onChange={handleChange}
+              value={
+                value?.unit5 ? Big(value.unit5).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
