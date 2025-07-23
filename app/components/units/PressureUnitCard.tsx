b0c96c6a4f5cc9fab9c429be9c0d6887bd6b1214
Piotr Cembrowski <losttape@gmail.com>
2025-07-23 15:43:39 +0200

    updating jsx tree

--- app/components/units/PressureUnitCard.tsx
+++ app/components/units/PressureUnitCard.tsx
@@ -87,19 +87,16 @@ export default function PressureUnitCard({ value, digit }: UnitCardProps) {
 
   return (
     <div className="group relative overflow-hidden rounded-lg border border-[#46A66F] bg-[#4F3E8C]  text-[#afa0e2]">
-      <div className="aspect-[4/3] md:aspect-auto p-3 flex flex-col justify-between space-y-2">
-        <div className="mb-8">
-          <h3 className="font-medium"></h3>
-        </div>
-        <div className="w-full">
+      <div className="aspect-[4/3] md:aspect-auto p-3">
+        <div className="mb-8"></div>
+        <div>
           <label>{unit1Name}</label>
           <br />
           <input
             name="unit1"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
             onChange={handleChange}
-            value={value?.unit1}
+            value={value?.unit1 ? Big(value.unit1).round(digit).toString() : ""}
           />
         </div>
         <div>
@@ -108,99 +105,122 @@ export default function PressureUnitCard({ value, digit }: UnitCardProps) {
           <input
             name="unit2"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
             onChange={handleChange}
-            value={value?.unit2}
+            value={value?.unit2 ? Big(value.unit2).round(digit).toString() : ""}
           />
         </div>
-        <div>
-          <label htmlFor="">{unit3Name}</label>
-          <br />
-          <input
-            name="unit3"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit3}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit4Name}</label>
-          <br />
-          <input
-            name="unit4"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit4}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit5Name}</label>
-          <br />
-          <input
-            name="unit5"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit5}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit6Name}</label>
-          <br />
-          <input
-            name="unit6"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit6}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit7Name}</label>
-          <br />
-          <input
-            name="unit7"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit7}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit8Name}</label>
-          <br />
-          <input
-            name="unit8"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit8}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit9Name}</label>
-          <br />
-          <input
-            name="unit9"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit9}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit10Name}</label>
-          <br />
-          <input
-            name="unit10"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit10}
-          />
-        </div>
+        {unit3Name && (
+          <div>
+            <label htmlFor="">{unit3Name}</label>
+            <br />
+            <input
+              name="unit3"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit3 ? Big(value.unit3).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit4Name && (
+          <div>
+            <label htmlFor="">{unit4Name}</label>
+            <br />
+            <input
+              name="unit4"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit4 ? Big(value.unit4).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit5Name && (
+          <div>
+            <label htmlFor="">{unit5Name}</label>
+            <br />
+            <input
+              name="unit5"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit5 ? Big(value.unit5).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit6Name && (
+          <div>
+            <label htmlFor="">{unit6Name}</label>
+            <br />
+            <input
+              name="unit6"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit6 ? Big(value.unit6).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit7Name && (
+          <div>
+            <label htmlFor="">{unit7Name}</label>
+            <br />
+            <input
+              name="unit7"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit7 ? Big(value.unit7).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit8Name && (
+          <div>
+            <label htmlFor="">{unit8Name}</label>
+            <br />
+            <input
+              name="unit8"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit8 ? Big(value.unit8).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit9Name && (
+          <div>
+            <label htmlFor="">{unit9Name}</label>
+            <br />
+            <input
+              name="unit9"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit9 ? Big(value.unit9).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
+        {unit10Name && (
+          <div>
+            <label htmlFor="">{unit10Name}</label>
+            <br />
+            <input
+              name="unit10"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit10 ? Big(value.unit10).round(digit).toString() : ""
+              }
+            />
+          </div>
+        )}
       </div>
     </div>
   );
