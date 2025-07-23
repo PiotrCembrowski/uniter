4d33d5feb997ab2600513093258f9914aaa13f66
Piotr Cembrowski <losttape@gmail.com>
2025-07-23 15:27:08 +0200

    jsx tree, choosing digit handler

--- app/components/units/PressureUnitDash.tsx
+++ app/components/units/PressureUnitDash.tsx
@@ -17,6 +17,7 @@ import { newDigitsState } from "@/store";
 
 const PressureUnitDash = () => {
   const showState = useAppSelector((state) => state.pressureSlice.unit);
+  const showDigit = useAppSelector((state) => state.digitSlice.digits);
 
   const unit = Big(showState.unit).toNumber();
 
@@ -26,24 +27,53 @@ const PressureUnitDash = () => {
   const [digit, setDigit] = useState<number>(4);
 
   useEffect(() => {
+    setDigit(showDigit);
+
     setValues({
-      unit1: showState,
-      unit2: showState / 100,
-      unit3: showState / 100000,
-      unit4: showState / 1000000,
-      unit5: showState / 9.80665,
-      unit6: showState / 98066.5,
-      unit7: showState / 101325,
-      unit8: showState / 133.322,
-      unit9: showState / 133.322,
-      unit10: showState / 6894.76,
+      unit1: unit,
+      unit2: unit / 100,
+      unit3: unit / 100000,
+      unit4: unit / 1000000,
+      unit5: unit / 9.80665,
+      unit6: unit / 98066.5,
+      unit7: unit / 101325,
+      unit8: unit / 133.322,
+      unit9: unit / 133.322,
+      unit10: unit / 6894.76,
     });
-  }, [showState]);
+  }, [unit, showDigit]);
+
+  const digitHandler = (value: string) => {
+    console.log("Selected digit:", value, digit);
+    setDigit(Number(value));
+    dispatch(newDigitsState(Number(value)));
+  };
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-      <PressureUnitCard value={values} />
-    </div>
+    <>
+      <h3 className="text-white flex">
+        Round up to the
+        <Select onValueChange={digitHandler}>
+          <SelectTrigger className="w-[90px]">
+            <SelectValue placeholder="4" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Array.from({ length: 15 }, (_, i) => (
+                <SelectItem key={i + 1} value={(i + 1).toString()}>
+                  {i + 1}
+                </SelectItem>
+              ))}
+              <SelectItem value="infinite">infinite</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>{" "}
+        digits after the decimal.
+      </h3>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <PressureUnitCard value={values} digit={digit} />
+      </div>
+    </>
   );
 };
 
