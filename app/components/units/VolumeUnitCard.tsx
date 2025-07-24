a2ba9a984aad1df2a3c6fabe87faeba7fe12e817
Piotr Cembrowski <losttape@gmail.com>
2025-07-24 15:45:50 +0200

    jsx tree finished

--- app/components/units/VolumeUnitCard.tsx
+++ app/components/units/VolumeUnitCard.tsx
@@ -348,16 +348,35 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
           baseValue = inputValue;
       }
 
-      dispatch(newAmericanVolumeState(baseValue));
-      dispatch(newImperialVolumeState(imperialValue));
-      dispatch(newMetricVolumeState(metricValue));
-      dispatch(newKitchenVolumeState(kitchenValue));
+      dispatch(
+        newImperialVolumeState({
+          unit: Big(imperialValue).toString(),
+        })
+      );
+
+      dispatch(
+        newMetricVolumeState({
+          unit: Big(metricValue).toString(),
+        })
+      );
+
+      dispatch(
+        newAmericanVolumeState({
+          unit: Big(baseValue).toString(),
+        })
+      );
+
+      dispatch(
+        newKitchenVolumeState({
+          unit: Big(kitchenValue).toString(),
+        })
+      );
     }
   };
 
   return (
-    <div className="group relative overflow-hidden rounded-lg border border-[#46A66F] bg-[#4F3E8C]  text-[#afa0e2] h-auto">
-      <div className="aspect-[4/3] md:aspect-auto p-3 flex flex-col justify-between space-y-2">
+    <div className="group relative overflow-hidden rounded-lg border border-[#46A66F] bg-[#4F3E8C]  text-[#afa0e2]">
+      <div className="aspect-[4/3] md:aspect-auto p-3">
         <div className="mb-8">
           <h3 className="font-medium">{title}</h3>
         </div>
@@ -367,9 +386,8 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
           <input
             name="unit1"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
             onChange={handleChange}
-            value={value?.unit1}
+            value={value?.unit1 ? Big(value.unit1).round(digit).toString() : ""}
             data-title={title}
           />
         </div>
@@ -379,48 +397,56 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
           <input
             name="unit2"
             className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
             onChange={handleChange}
-            value={value?.unit2}
+            value={value?.unit2 ? Big(value.unit2).round(digit).toString() : ""}
             data-title={title}
           />
         </div>
-        <div>
-          <label htmlFor="">{unit3Name}</label>
-          <br />
-          <input
-            name="unit3"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit3}
-            data-title={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit4Name}</label>
-          <br />
-          <input
-            name="unit4"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit4}
-            data-title={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="">{unit5Name}</label>
-          <br />
-          <input
-            name="unit5"
-            className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-            type="text"
-            onChange={handleChange}
-            value={value?.unit5}
-            data-title={title}
-          />
-        </div>
+        {unit3Name && (
+          <div>
+            <label htmlFor="">{unit3Name}</label>
+            <br />
+            <input
+              name="unit3"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit3 ? Big(value.unit3).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
+        {unit4Name && (
+          <div>
+            <label htmlFor="">{unit4Name}</label>
+            <br />
+            <input
+              name="unit4"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit4 ? Big(value.unit4).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
+        {unit5Name && (
+          <div>
+            <label htmlFor="">{unit5Name}</label>
+            <br />
+            <input
+              name="unit5"
+              className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
+              onChange={handleChange}
+              value={
+                value?.unit5 ? Big(value.unit5).round(digit).toString() : ""
+              }
+              data-title={title}
+            />
+          </div>
+        )}
         {unit6Name && (
           <div>
             <label htmlFor="">{unit6Name}</label>
@@ -428,9 +454,10 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
             <input
               name="unit6"
               className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-              type="text"
               onChange={handleChange}
-              value={value?.unit6}
+              value={
+                value?.unit6 ? Big(value.unit6).round(digit).toString() : ""
+              }
               data-title={title}
             />
           </div>
@@ -442,9 +469,10 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
             <input
               name="unit7"
               className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-              type="text"
               onChange={handleChange}
-              value={value?.unit7}
+              value={
+                value?.unit7 ? Big(value.unit7).round(digit).toString() : ""
+              }
               data-title={title}
             />
           </div>
@@ -456,9 +484,10 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
             <input
               name="unit8"
               className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-              type="text"
               onChange={handleChange}
-              value={value?.unit8}
+              value={
+                value?.unit8 ? Big(value.unit8).round(digit).toString() : ""
+              }
               data-title={title}
             />
           </div>
@@ -470,9 +499,10 @@ export default function VolumeUnitCard({ title, value, digit }: UnitCardProps) {
             <input
               name="unit9"
               className="w-max-[100%] w-[100%] box-border border-2 border-[#9177F2] bg-[#4F3E8C] text-[#46A66F] font-bold pl-1"
-              type="text"
               onChange={handleChange}
-              value={value?.unit9}
+              value={
+                value?.unit9 ? Big(value.unit9).round(digit).toString() : ""
+              }
               data-title={title}
             />
           </div>
