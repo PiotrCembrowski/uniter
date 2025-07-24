39cd432e91d66060f33c1280f1f714c49c56a261
Piotr Cembrowski <losttape@gmail.com>
2025-07-24 14:13:27 +0200

    finishing unitdash

--- app/components/units/PowerUnitDash.tsx
+++ app/components/units/PowerUnitDash.tsx
@@ -17,33 +17,65 @@ import { newDigitsState } from "@/store";
 
 const PowerUnitDash = () => {
   const showState = useAppSelector((state) => state.powerSlice.units);
+  const showDigit = useAppSelector((state) => state.digitSlice.digits);
 
-  const [values, setValues] = useState<Values>({
-    unit1: showState,
-    unit2: showState / 1000,
-    unit3: showState / 1000000,
-    unit4: showState / 745.7,
-    unit5: showState / 0.293071,
-    unit6: showState / 17.5843,
-    unit7: showState / 1055.06,
-  });
+  const stateValue = Big(showState.units).toNumber();
+
+  const [values, setValues] = useState<Values>();
+  const [digit, setDigit] = useState<number>(4);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    setDigit(showDigit);
+
     setValues({
-      unit1: showState,
-      unit2: showState / 1000,
-      unit3: showState / 1000000,
-      unit4: showState / 745.7,
-      unit5: showState / 0.293071,
-      unit6: showState / 17.5843,
-      unit7: showState / 1055.06,
+      unit1: stateValue,
+      unit2: stateValue / 1000,
+      unit3: stateValue / 1000000,
+      unit4: stateValue / 745.7,
+      unit5: stateValue / 0.293071,
+      unit6: stateValue / 17.5843,
+      unit7: stateValue / 1055.06,
     });
-  }, [showState]);
+  }, [stateValue, showDigit]);
+
+  const digitHandler = (value: string) => {
+    console.log("Selected digit:", value, digit);
+    if (value === "infinite") {
+      setDigit(100);
+      dispatch(newDigitsState(100));
+      return;
+    }
+    setDigit(Number(value));
+    dispatch(newDigitsState(Number(value)));
+  };
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-      <PowerUnitCard value={values} />
-    </div>
+    <>
+      <h3 className="text-white flex">
+        Round up to the
+        <Select onValueChange={digitHandler}>
+          <SelectTrigger className="w-[90px]">
+            <SelectValue placeholder="4" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Array.from({ length: 15 }, (_, i) => (
+                <SelectItem key={i + 1} value={(i + 1).toString()}>
+                  {i + 1}
+                </SelectItem>
+              ))}
+              <SelectItem value="infinite">infinite</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>{" "}
+        digits after the decimal.
+      </h3>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <PowerUnitCard value={values} digit={digit} />
+      </div>
+    </>
   );
 };
 
