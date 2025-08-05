5b8233ce896338781c8cb3cb7968047db4eaf24e
Piotr Cembrowski <losttape@gmail.com>
2025-08-05 15:35:16 +0200

    deleting not used imports

--- app/components/header/HeaderMain.tsx
+++ app/components/header/HeaderMain.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Bell, Grid } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import {
   DropdownMenu,
