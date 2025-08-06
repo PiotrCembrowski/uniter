5258ce7494535e948b90da556d9a34dbc5478f39
Piotr Cembrowski <losttape@gmail.com>
2025-08-06 18:06:21 +0200

    updating google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Uniter",
+  title: "Unit Converter",
   description: "Unit converter",
 };
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           <StoreProvider>{children}</StoreProvider>
         </main>
       </body>
+      <GoogleAnalytics gaId="G-ZLDWN9CHBL" />
     </html>
   );
 }
